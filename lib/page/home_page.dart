009de05de868dad950a9cb752437755c293fdc6c
kuy <kuy472013@gmail.com>
2024-03-05 01:05:49 +0900

    page 분리

---
+++ lib/page/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_study/widget/badge_widget.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('home'),
+      ),
+      body: const Center(
+        child: BadgeWidget(text: '요약'),
+      ),
+    );
+  }
+}
