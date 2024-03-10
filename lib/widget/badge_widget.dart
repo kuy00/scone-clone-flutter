e9d03d4753d5bb8763994bcea90d304739ceb92d
kuy <kuy472013@gmail.com>
2024-03-10 22:19:29 +0900

    badge widget 스타일 수정

--- lib/widget/badge_widget.dart
+++ lib/widget/badge_widget.dart
@@ -8,15 +8,15 @@ class BadgeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 66,
+      height: 24,
+      padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(50),
         color: Colors.grey,
       ),
       child: Text(
         text,
-        style: const TextStyle(
-            color: Colors.white, fontSize: 40),
+        style: const TextStyle(color: Colors.white, fontSize: 14),
       ),
     );
   }
