2726ceefdbd4f7fe34260553b56299d561ff3308
kuy <kuy472013@gmail.com>
2024-05-01 19:46:07 +0900

    라우트 애니메이션 수정

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_study/page/add_history_page.dart';
 import 'package:flutter_study/page/home_page.dart';
 import 'package:go_router/go_router.dart';
@@ -12,28 +12,12 @@ final GoRouter routes = GoRouter(
       },
       routes: <RouteBase>[
         GoRoute(
-          path: 'plan/:id/addHistory',
-          pageBuilder: (BuildContext context, GoRouterState state) =>
-              CustomTransitionPage(
-                  child: AddHistoryPage(
-                    planId: state.pathParameters['id']!,
-                  ),
-                  transitionsBuilder: (
-                    BuildContext context,
-                    Animation<double> animation,
-                    Animation<double> secondaryAnimation,
-                    Widget child,
-                  ) =>
-                      SlideTransition(
-                        position: animation.drive(
-                          Tween<Offset>(
-                            begin: const Offset(1.25, 0),
-                            end: Offset.zero,
-                          ).chain(CurveTween(curve: Curves.easeIn)),
-                        ),
-                        child: child,
-                      )),
-        )
+            path: 'plan/:id/addHistory',
+            pageBuilder: (BuildContext context, GoRouterState state) =>
+                CupertinoPage(
+                    child: AddHistoryPage(
+                  planId: state.pathParameters['id']!,
+                )))
       ],
     ),
   ],
